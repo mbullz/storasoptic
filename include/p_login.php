c7dbb743908e525996c7d9d9af25275d8991e916
Robert Tio <m_bull_z@yahoo.com>
2019-04-13 23:10:47 +0700

    update

--- include/p_login.php
+++ include/p_login.php
@@ -3,44 +3,7 @@ session_start();
 include('config_db.php');
 $nik    = $mysqli->real_escape_string($_POST['username']);
 $pass   = $mysqli->real_escape_string($_POST['password']);
-// check login
-/*$query_login = "select kode, kontak, akses from kontak where kode='$nik' AND pass='$pass' AND aktif='1'";
-$login = $mysqli->query($query_login);
-$row_login = mysqli_fetch_assoc($login);
-$total_login = mysqli_num_rows($login);
-if($total_login > 0) { ?>*/
 
-	if (($nik === 'efata' && $pass === 'efata123') || ($nik === 'admin' && $pass === 'admin123')) 
-	{
-		$akses = $mysqli->query("select user_id,akses from kontak where user_id=1");
-		$row_akses = mysqli_fetch_assoc($akses);
-		$_SESSION['i_sesadmin'] = 1;
-		$_SESSION['nama'] = $nik;
-		$_SESSION['akses'] = $row_akses['akses'];
-		$_SESSION['user_id'] = $row_akses['user_id'];
-		?>
-    		<script type="text/javascript">
-        		alert('Login Berhasil ...');
-        		location.href = '/<?=$base_url?>/index.php';
-    		</script>
-		<?php
-	}
-	else if (($nik === 'operator' && $pass === 'operator123')) 
-	{
-		$akses = $mysqli->query("select akses from kontak where user_id=1");
-		$row_akses = mysqli_fetch_assoc($akses);
-		$_SESSION['i_sesadmin'] = 0;
-		$_SESSION['nama'] = $nik;
-		$_SESSION['akses'] = $row_akses['akses'];
-		?>
-    		<script type="text/javascript">
-        		alert('Login Berhasil ...');
-        		location.href = '/<?=$base_url?>/index.php';
-    		</script>
-		<?php
-	}
-	else
-	{
 		$pass = md5($pass);
 		$rs2 = $mysqli->query("SELECT * FROM kontak WHERE kontak LIKE '$nik' AND pass LIKE '$pass'");
 		if ($data2 = mysqli_fetch_assoc($rs2))
@@ -54,7 +17,7 @@ if($total_login > 0) { ?>*/
 			?>
 				<script type="text/javascript">
 					alert('Login Berhasil ...');
-					location.href = '/<?=$base_url?>/index.php';
+					location.href = '<?=$base_url?>index.php';
 				</script>
 			<?php
 		}
@@ -66,5 +29,5 @@ if($total_login > 0) { ?>*/
 				</script>
 			<?php
 		}
-	}
+
 ?>
